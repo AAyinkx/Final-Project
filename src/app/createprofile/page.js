804531b477ef50763948ba5824cf2228a512aeb3
Ashir-Valjee <ashir@eco-motive.com>
2024-11-08 11:48:21 +0000

    fixed profile query

--- src/app/createprofile/page.js
+++ src/app/createprofile/page.js
@@ -1,10 +1,17 @@
 import CreateProfileForm from "@/components/createProfileForm";
+import { auth, currentUser } from "@clerk/nextjs/server";
 
-export default function createProfile() {
+export default async function createProfile() {
+  const user = await currentUser();
   return (
     <>
       <h1> Create Profile </h1>
-      <CreateProfileForm />
+      <CreateProfileForm
+        clerk_id={user.id}
+        username={user.username}
+        first_name={user.firstName}
+        last_name={user.lastName}
+      />
     </>
   );
 }
