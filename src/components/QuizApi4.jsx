c034ef6f578d429135ef8871b9ab58d1d69aa3c1
Ashir-Valjee <ashir@eco-motive.com>
2024-11-11 15:54:00 +0000

    did some crazy stuff

--- src/components/QuizApi4.jsx
+++ src/components/QuizApi4.jsx
@@ -3,18 +3,18 @@ import { randomShuffle } from "@/utils/randomShuffle";
 import { decode } from "html-entities";
 import { useEffect, useState } from "react";
 export default function QuizApi4() {
-  const numberOfQuestions = 10;
+  const numberOfQuestions = 5;
   const [questions, setQuestions] = useState([]);
 
   useEffect(() => {
     async function getQuestions() {
       // !change port number if needed
       const response = await fetch(
-        `http://localhost:3001/api/quiz/?q=${numberOfQuestions}`
+        `http://localhost:3000/api/quiz/?q=${numberOfQuestions}`
       );
       const data = await response.json();
       const wrangledData = data.results;
-      setQuestions([wrangledData]);
+      setQuestions([wrangledData[0]]);
     }
     getQuestions();
   }, []);
@@ -26,7 +26,32 @@ export default function QuizApi4() {
   return (
     <>
       <h1>Quiz Api</h1>
-      <p>{questions[0].type}</p>
+      {/* {questions.map((question, index) => (
+        <div key={index}>
+          <p>{question.question}</p>
+        </div>
+      ))} */}
+
+      {questions.map((question, index) => {
+        let answers = [
+          question.correct_answer,
+          question.incorrect_answers[0],
+          question.incorrect_answers[1],
+          question.incorrect_answers[2],
+        ];
+        let shuffledAnswers = randomShuffle(answers);
+        return (
+          <div key={index}>
+            <h1>Question&#58; {question.question}</h1>
+
+            <p>Answer&#58; {question.correct_answer}</p>
+            <p>{shuffledAnswers[0]}</p>
+            <p>{shuffledAnswers[1]}</p>
+            <p>{shuffledAnswers[2]}</p>
+            <p>{shuffledAnswers[3]}</p>
+          </div>
+        );
+      })}
     </>
   );
 }
--- src/utils/randomShuffle.js
+++ src/utils/randomShuffle.js
@@ -1,5 +1,5 @@
 export function randomShuffle(array) {
-  const shuffledArray = array.sort((a, b) => 0.5 - Math.random());
+  const shuffledArray = array.sort(() => 0.5 - Math.random());
   return shuffledArray;
 }
 // console.log(randomShuffle());
